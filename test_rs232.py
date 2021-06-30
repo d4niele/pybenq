dda533f46172e6c15a9023af65ff5295c86a89b3
Daniele Rizzo <calupietru@gmail.com>
2021-06-30 11:48:38 +0200

    Update test_rs232.py

--- test_rs232.py
+++ test_rs232.py
@@ -1,11 +1,12 @@
+from flask import Flask
 import serial
 
-s = serial.Serial('/dev/ttyUSB1',baudrate=115200)
+s = serial.Serial('/dev/ttyUSB0',baudrate=115200)
 
 hexCodes = {
     "on":bytearray('\r*pow=on#\r','ascii'),
     "off":bytearray('\r*pow=off#\r','ascii'),
-   };
+   }
 
 def _send_command(k):
     if k in hexCodes:
@@ -18,3 +19,18 @@ def prj_on():
 
 def prj_off():
     _send_command('off')
+
+app = Flask(__name__)
+
+@app.route("/benq_off")
+def benq_off():
+    prj_off()    
+    return 'ok'
+
+@app.route("/benq_on")
+def benq_on():
+    prj_on()    
+    return 'ok'
+
+if __name__ == "__main__":
+    app.run('0.0.0.0')
